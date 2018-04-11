b049f26ff8bdfde459afd1aa2223c1eaa97d859c
GuilleGaete <guillermo@tiendanube.com>
2018-04-11 15:03:47 -0300

    change ports

--- src/js/actions/roomActions.js
+++ src/js/actions/roomActions.js
@@ -12,7 +12,7 @@ export function fetchRoom(roomId) {
       - post some tweets to http://rest.learncode.academy/api/yourusername/tweets
     */
     //TODO levantar port de .env
-    axios.get(`http://localhost:3555/api/rooms/${roomId}`)
+    axios.get(`http://localhost:3556/api/rooms/${roomId}`)
       .then((response) => {
         const helper = new actionHelper(response.data)
         response.data.state=helper.getCurrentState()
