ca5a08063971d6080b47c9f3a4148ec059d001d0
Virginia Milano <virginia@tiendanube.com>
2018-04-11 11:40:39 -0300

    loading + times

--- src/js/components/Info/Info.js
+++ src/js/components/Info/Info.js
@@ -5,14 +5,36 @@ import Button from "../Button/Button"
 
 
 export default class Info extends Component {
+   
+    buildTimeLabel(state, time){
+	    switch (state) {
+	      case "toBusy":
+	        return `${time}a`
+	        break;
+	      case "busy":
+	        return `${time}`
+	        break;
+	      case "toFree":
+	        return `${time}a`
+	        break;
+	      case "free":
+	        return "libre"
+	        break;
+	      default: 
+	        return ""
+    	}
+  	}
+
+
     render(){
-   	    const { label, state, time, user} = this.props
+   	    const { label, state, user, time} = this.props
    	    const button={ state }
+   	    let timeLabel = this.buildTimeLabel(state, time)
 
     	return (    
 			<div className="info">
             	<div className="label">{label}</div>
-            	<div className="time">{time}</div>
+            	<div className="time">{timeLabel}</div>
             	<div className="user">por <strong>@{user}</strong></div>
 
             	<Button {...button}/>
