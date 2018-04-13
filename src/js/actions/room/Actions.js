74b67bd98c80b7d1f3575b80b1707c033f287a7d
GuilleGaete <guillermo@tiendanube.com>
2018-04-13 02:58:52 -0300

    fix book_action events

--- src/js/actions/room/Actions.js
+++ src/js/actions/room/Actions.js
@@ -59,19 +59,24 @@ function fetchRoom(roomId) {
   }
 }
 
-function bookRoom(time) {
+function bookRoom(roomId,time) {
   return function(dispatch) {
     dispatch({type: "BOOK_ACTION"})
-
+    const params=roomId.replace("sala-", "?number=")
     //'/api/rooms/:room/:time'
     axios.post(`http://${window.location.hostname}/api/rooms/sala-8/${time}`)
       .then((response) => {
+        const helper = new actionHelper(response.data)
+        response.data.state=helper.currentState()
+        response.data.currentSlot=helper.currentSlot()
+        response.data.nextFreeSlot=helper.nextFreeSlot()
+        response.data.nextMeeting=helper.nextMeeting()
+        response.data.state.ocupationState=helper.ocupationState(response.data.currentSlot)
         dispatch({type: "FETCH_ROOM_FULFILLED", payload: response.data})
-        const organizer=response.data.state.currentSlot.organizer
-        //TODO fix it harcoded value
-        if(true){
+        const user=response.data.currentSlot.organizer
+        if(user){
           dispatch({type: "FETCH_USER"});
-          axios.get(`http://${window.location.hostname}/api/user/${"guillermo@tiendanube.com"}`)
+          axios.get(`http://${window.location.hostname}/api/user/${user.email}`)
             .then((response) => {
               const payload={
                 name:response.data.slackUser.name,
@@ -87,7 +92,25 @@ function bookRoom(time) {
               dispatch({type: "FETCH_USER_REJECTED", payload: err})
             })
         }
-
+        const nextMeetingOwner=response.data.nextMeeting.organizer
+        if(nextMeetingOwner){
+          dispatch({type: "FETCH_NEXT_METTING_OWNER"});
+          axios.get(`http://${window.location.hostname}/api/user/${nextMeetingOwner.email}`)
+            .then((response) => {
+              const payload={
+                name:response.data.slackUser.name,
+                display_name:response.data.slackUser.profile.display_name,
+                images:{
+                  original:response.data.slackUser.profile.image_original,
+                  size_72:response.data.slackUser.profile.image_72
+                }
+              }
+              dispatch({type: "FETCH_NEXT_METTING_OWNER_FULFILLED", payload})
+            })
+            .catch((err) => {
+              dispatch({type: "FETCH_NEXT_METTING_OWNER_REJECTED", payload: err})
+            })
+        }
       })
       .catch((err) => {
         dispatch({type: "BOOK_ACTION_REJECTED", payload: err})
