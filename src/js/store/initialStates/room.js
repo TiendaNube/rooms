7f030adf80b72ef61e7df2662baa80ff0db35a7f
GuilleGaete <guillermo@tiendanube.com>
2018-04-14 15:48:54 -0300

    Fix initial state logic

--- src/js/store/initialStates/room.js
+++ src/js/store/initialStates/room.js
@@ -1,43 +1,14 @@
-const initialState={
-      data:{
-        schedule: [],
-        name: null,
-        state: {
-          status:null,
-          ocupationState:null,
-          time:null
-        },
-        currentSlot:{
-          booking:false,
-          booked:false,
-          cancelling:false,
-          cancelled:false,
-          available:null,
-          end:null,
-          organizer:null,
-          private:null,
-          start:null,
-          summary:null,
-          error:null
-        },
-        nextFreeSlot:{
-          end:null,
-          organizer:null,
-          private:null,
-          start:null,
-          summary:null
-        },
-        nextMeeting:{
-          end:null,
-          organizer:null,
-          private:null,
-          start:null,
-          summary:null
-        }
-      },
+const propertiesToFronEnd={
       fetching: false,
       fetched: false,
       error: null
 }
+const initialState={
+      stateRoom:null
+}
+const initialStateToFrontEnd={...initialState,...propertiesToFronEnd}
 
-export default initialState
+module.exports={
+  initialState,
+  initialStateToFrontEnd
+}
